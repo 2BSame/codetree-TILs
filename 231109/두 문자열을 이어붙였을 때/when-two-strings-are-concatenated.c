32ed9d5cce0c8f0146b9f5fa713bc03b4dd656ec
2BSame <145317726+2BSame@users.noreply.github.com>
2023-11-09 13:57:13 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 231109/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char arr[2][100];
+    char std[2][100];
+    int i=0;
+    for(i=0;i<2;i++){
+        scanf("%s",arr[i]);
+    }
+    strcat(std[0],arr[0]);
+    strcat(std[0],arr[1]);
+    strcat(std[1],arr[1]);
+    strcat(std[1],arr[0]);
+    if(strcmp(std[0], std[1])==0)
+        printf("true");
+    else
+        printf("false");
+    return 0;
+}
