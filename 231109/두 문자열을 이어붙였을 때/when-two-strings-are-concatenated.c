e14d232d2b70358f29af5f7dde3344eb49965c83
2BSame <145317726+2BSame@users.noreply.github.com>
2023-11-09 14:18:29 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231109/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.c
+++ 231109/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.c
@@ -3,17 +3,17 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     char arr[2][100];
-    char std[2][100];
+    char std[2][200];
     int i=0;
     for(i=0;i<2;i++){
         scanf("%s",arr[i]);
     }
     //printf("%s",arr[0]);
-    strcat(std[0],arr[0]);
-    strcat(std[0],arr[1]);
-    strcat(std[1],arr[1]);
-    strcat(std[1],arr[0]);
-    if(strcmp(std[0], std[1])==0)
+    strcpy(std[0],arr[0]);
+    strcpy(std[1],arr[1]);
+    strcat(std[0],std[1]);
+    strcat(arr[1],arr[0]);
+    if(strcmp(std[0], arr[1])==0)
         printf("true");
     else
         printf("false");
