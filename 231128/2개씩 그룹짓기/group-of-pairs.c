d88a0818b66c50abce33fb1410e59b186aec8a35
2BSame <145317726+2BSame@users.noreply.github.com>
2023-11-28 20:14:26 +0900

    Title: 2개씩 그룹짓기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231128/2개씩 그룹짓기/group-of-pairs.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 그룹의 최소 합과 최대 합을 찾는 함수
+void find_min_max_sum(int *nums, int n, int *min_sum, int *max_sum) {
+    *min_sum = INT_MAX;
+    *max_sum = INT_MIN;
+
+    for (int i = 0; i < n / 2; i++) {
+        int group_sum = nums[i] + nums[n - 1 - i];
+        *min_sum = group_sum < *min_sum ? group_sum : *min_sum;
+        *max_sum = group_sum > *max_sum ? group_sum : *max_sum;
+    }
+}
+
+// 최대 합을 최소화하는 함수
+int minimize_max_group_sum(int N, int *elements) {
+    int min_sum, max_sum;
+
+    // 주어진 원소를 정렬
+    qsort(elements, 2 * N, sizeof(int), compare);
+
+    // 최소 합과 최대 합을 찾음
+    find_min_max_sum(elements, 2 * N, &min_sum, &max_sum);
+
+    return max_sum;
+}
+
+// 비교 함수
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    int *elements = (int *)malloc(2 * N * sizeof(int));
+
+    for (int i = 0; i < 2 * N; i++) {
+        scanf("%d", &elements[i]);
+    }
+
+    int result = minimize_max_group_sum(N, elements);
+    printf("%d\n", result);
+
+    free(elements);
+
+    return 0;
+}
