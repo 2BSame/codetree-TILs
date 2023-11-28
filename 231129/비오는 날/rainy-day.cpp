4483bd1bfef859bce24f998e368fd6caa06c6d29
2BSame <145317726+2BSame@users.noreply.github.com>
2023-11-29 01:33:14 +0900

    Title: 비오는 날, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231129/비오는 날/rainy-day.cpp
+++ 231129/비오는 날/rainy-day.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <climits>
 
 using namespace std;
 
@@ -15,27 +14,35 @@ public:
         : date(date), day(day), weather(weather) {}
 };
 
+int compareDates(const string &date1, const string &date2) {
+    return date1.compare(date2);
+}
+
 int main() {
     int n;
     cin >> n;
 
     vector<WeatherData> data;
-    int minRainIndex = INT_MAX;
+    string earliestRainDate = "9999-12-31";  // 초기값을 날짜 범위에서 가장 늦게 설정
 
     for (int i = 0; i < n; i++) {
         string date, day, weather;
         cin >> date >> day >> weather;
         data.push_back(WeatherData(date, day, weather));
 
-        // 비가 오는 경우, 가장 빠른 날짜의 인덱스를 저장
-        if (weather == "Rain" && i < minRainIndex) {
-            minRainIndex = i;
+        // 비가 오는 경우, 가장 빠른 날짜를 저장
+        if (weather == "Rain" && compareDates(date, earliestRainDate) < 0) {
+            earliestRainDate = date;
         }
     }
 
     // 가장 빠른 날짜의 비 정보 출력
-    WeatherData earliestRain = data[minRainIndex];
-    cout << earliestRain.date << " " << earliestRain.day << " " << earliestRain.weather << endl;
+    for (const auto &entry : data) {
+        if (entry.date == earliestRainDate) {
+            cout << entry.date << " " << entry.day << " " << entry.weather << endl;
+            break;
+        }
+    }
 
     return 0;
 }
