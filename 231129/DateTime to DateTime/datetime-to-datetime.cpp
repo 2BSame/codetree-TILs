d08fb0762306c3b96af1acbdba41cb9521962e0d
2BSame <145317726+2BSame@users.noreply.github.com>
2023-11-29 16:14:20 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231129/DateTime to DateTime/datetime-to-datetime.cpp
+++ 231129/DateTime to DateTime/datetime-to-datetime.cpp
@@ -25,7 +25,12 @@ int main() {
     }
     cout<<cnt;
     }
-    else
-    cout<<"-1";
+    else{
+        if(a==b&&b==c&&c==a)
+        cout<<"0";
+        else
+        cout<<"-1";
+
+    }
     return 0;
 }
