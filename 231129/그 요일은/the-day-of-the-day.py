aeea118612ec97e4fd0f0162b37d8e409fe6a26c
2BSame <145317726+2BSame@users.noreply.github.com>
2023-11-29 17:42:47 +0900

    Title: 그 요일은, Time: 153ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 231129/그 요일은/the-day-of-the-day.py
+++ 231129/그 요일은/the-day-of-the-day.py
@@ -1,27 +1,22 @@
-def count_occurrences(m1, d1, m2, d2, start_day):
-    days_in_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+from datetime import datetime, timedelta
 
-    days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-    start_index = days_of_week.index(start_day)
+# 입력 받기
+m1, d1, m2, d2 = map(int, input().split())
+start_date = datetime(2024, m1, d1)
+end_date = datetime(2024, m2, d2)
+day_of_week = input()
 
-    total_days = 0
-    for month in range(m1, m2 + 1):
-        end_day = days_in_month[month] if month != m2 else d2
-        for day in range(1, end_day + 1):
-            total_days += 1
-            if total_days % 7 == 0:
-                start_index = (start_index + 1) % 7
+# 요일 문자열을 숫자로 매핑
+day_mapping = {'Mon': 0, 'Tue': 1, 'Wed': 2, 'Thu': 3, 'Fri': 4, 'Sat': 5, 'Sun': 6}
+target_day = day_mapping[day_of_week]
 
-    return start_index
+# 날짜 범위 내에서 요일이 몇 번 등장하는지 계산
+count = 0
+current_date = start_date
+while current_date <= end_date:
+    if current_date.weekday() == target_day:
+        count += 1
+    current_date += timedelta(days=1)
 
-
-def main():
-    m1, d1, m2, d2 = map(int, input().split())
-    start_day = input().strip()
-
-    occurrences = count_occurrences(m1, d1, m2, d2, start_day)
-    print(occurrences)
-
-
-if __name__ == "__main__":
-    main()
+# 결과 출력
+print(count)
