eb36ab12626a5d44fc1500fef85f4360a3e45261
2BSame <145317726+2BSame@users.noreply.github.com>
2023-11-29 16:05:00 +0900

    Title: DateTime to DateTime, Time: 1098ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 231129/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c;
+    int cnt=0;
+    cin>>a>>b>>c;
+    int d=11;
+    int h=11;
+    int m=11;
+    while(true){
+        if(a==d&&b==h&&c==m)
+        break;
+        m++;
+        cnt++;
+        if(m>=60){
+            h++;
+            m=0;
+        }
+        if(h>=24){
+            d++;
+            h=0;
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
