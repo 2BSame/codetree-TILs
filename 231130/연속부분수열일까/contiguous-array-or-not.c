3b8ee7611b95d0992d81930663f861afefd16548
2BSame <145317726+2BSame@users.noreply.github.com>
2023-11-30 23:20:14 +0900

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231130/연속부분수열일까/contiguous-array-or-not.c
+++ 231130/연속부분수열일까/contiguous-array-or-not.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+int judge(int n,int m,int *a,int *b){
+    int i,j;
+    int cun=0;
+    for(i=0;i<n;i++){
+        if(a[i]==b[cun]){
+            cun++;
+            if(cun==m)
+            return cun;
+        }
+        else{
+            cun=0;
+        }
+    }
+    return cun;
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
     int n,m;
     int i,j;
-    int cun=0;
     int *str=NULL;
     int *str1=NULL;
     scanf("%d %d",&n,&m);
@@ -16,21 +31,11 @@ int main() {
     for(i=0;i<m;i++){
         scanf("%d",&str1[i]);
     }
-    for(i=0;i<n;i++){
-        if(str[i]==str[cun]){
-            cun++;
-            if(cun==m)
-            break;
-        }
-        if(str[i]!=str[cun]){
-            cun=0;
-        }
-    }
-    free(str);
-    free(str1);
-    if(cun==m)
+    if(judge(n,m,str,str1)==m)
         printf("Yes");
     else
         printf("No");
+    free(str);
+    free(str1);
     return 0;
 }
